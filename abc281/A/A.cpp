7a31fbc9fc0e60296f2adb285cd92cec0efdc73a
cupcake08 <bhankhariaa@gmail.com>
2022-12-29 00:16:11 +0530

    :sparkles: Temple File Added For Each File

---
+++ abc281/A/A.cpp
@@ -0,0 +1,53 @@
+/**
+		*
+		* Author      : Ankit Bhankharia
+		* Created At  : 2022-12-29 0:15:16
+		* Contest     : AtCoder Beginner Contest 281
+		* Problem     : Count Down
+		*
+		**/
+		
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOfast                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define nl '\n'
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sz(x) (int)x.size()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define per(i, a, b) for (int i = a; i >= b; i--)
+#define endl '\n'
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vin(v, n) \
+    vi v(n);     \
+    for(auto &i:v) cin >> i;
+#define vout(v)           \
+    for (auto i : v)      \
+        cout << i << " "; \
+    cout << endl;
+
+const int MOD = 1e9 + 7;
+
+void solve(){
+}
+
+int main()
+{
+    IOfast;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
--- main.go
+++ main.go
@@ -3,23 +3,35 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly/v2"
 )
 
-const URL = "https://atcoder.jp"
+const (
+	URL = "https://atcoder.jp"
+	// your c++ template file path
+	TEMPLATE = "/home/ankit/CP/template.cpp"
+)
 
 type Problem struct {
 	Prefix string
-	Uri string
-	Name string
+	Uri    string
+	Name   string
 }
 
-func create_file(filename,content string) {
-	f,err := os.Create(filename)
+type Contest struct {
+	Colly *colly.Collector
+	Name  string
+	Base  string
+}
+
+func create_file(filename, content string) {
+	f, err := os.Create(filename)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -27,64 +39,144 @@ func create_file(filename,content string) {
 	f.WriteString(content + "\n")
 }
 
-func getTestCases(uri string,c *colly.Collector) {
+func (p *Problem) InitTemplateFile(contestName string) {
+	f, err := os.Open(TEMPLATE)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+
+	bytes, err := io.ReadAll(f)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	currentTime := time.Now()
+	// initial header
+	header := fmt.Sprintf(`/**
+		*
+		* Author      : Ankit Bhankharia
+		* Created At  : %d-%d-%d %d:%d:%d
+		* Contest     : %s
+		* Problem     : %s
+		*
+		**/
+		`,
+		currentTime.Year(),
+		currentTime.Month(),
+		currentTime.Day(),
+		currentTime.Hour(),
+		currentTime.Minute(),
+		currentTime.Second(),
+		contestName,
+		p.Name,
+	)
+
+	idx := len(header)
+	idx++
+
+	file, err := os.Create(p.Prefix + ".cpp")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer file.Close()
+
+	file.WriteString(header + "\n")
+
+	_, err = file.WriteAt(bytes, int64(idx))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	err = file.Sync()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func (pb *Problem) GetTestCases(uri string, c *colly.Collector) {
 	d := c.Clone()
-	d.OnHTML("span.lang > span.lang-en",func(h *colly.HTMLElement) {
+
+	d.OnHTML("span.lang > span.lang-en", func(h *colly.HTMLElement) {
 		ic := 0
 		oc := 0
-		h.ForEach("div.part > section",func(i int, h *colly.HTMLElement) {
+		h.ForEach("div.part > section", func(i int, h *colly.HTMLElement) {
 			// skip first four nodes
 			if i > 3 {
-			    // even case for input
-			    // odd case for output
+				// even case for input
+				// odd case for output
 				text := h.ChildText("pre")
-				if i % 2 == 0 {
-					filename := fmt.Sprintf("input_%d.txt",ic)
-					create_file(filename,text)
+				if i%2 == 0 {
+					filename := fmt.Sprintf("input_%d.txt", ic)
+					create_file(filename, text)
 					ic++
-				}else {
-					filename := fmt.Sprintf("output_%d.txt",oc)
-					create_file(filename,text)
+				} else {
+					filename := fmt.Sprintf("output_%d.txt", oc)
+					create_file(filename, text)
 					oc++
 				}
 			}
 		})
 	})
+
 	d.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting:",r.URL)
+		fmt.Println("Visiting:", r.URL)
 	})
+
 	d.Visit(URL + uri)
 }
 
-func getProblems(c *colly.Collector,name string) {
-	d := c.Clone()
+func (contest *Contest) ScrapeAtcoder() {
+	d := contest.Colly.Clone()
 
-	d.OnHTML("div.panel > table",func(h *colly.HTMLElement) {
-		h.ForEach("tbody > tr",func(i int, h *colly.HTMLElement) {
+	d.OnHTML("nav > div.container-fluid > div#navbar-collapse", func(h *colly.HTMLElement) {
+		contest.Name = h.ChildText("ul > li > a.contest-title")
+	})
+
+	d.OnHTML("div.panel > table", func(h *colly.HTMLElement) {
+		h.ForEach("tbody > tr", func(i int, h *colly.HTMLElement) {
 			pCode := h.ChildText("td.text-center")
 			pb := Problem{
 				Prefix: pCode,
-				Uri: h.ChildAttr("td > a","href"),
-				Name: strings.TrimPrefix(h.ChildText("td > a"),pCode),
+				Uri:    h.ChildAttr("td > a", "href"),
+				Name:   strings.TrimPrefix(h.ChildText("td > a"), pCode),
 			}
-			os.Mkdir(pb.Prefix,0777)
+			os.Mkdir(pb.Prefix, 0777)
 			os.Chdir(pb.Prefix)
-			getTestCases(pb.Uri,d)
+			pb.GetTestCases(pb.Uri, d)
+			pb.InitTemplateFile(contest.Name)
 			os.Chdir("..")
 		})
 	})
 
 	d.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting:",r.URL)
+		fmt.Println("Visiting:", r.URL)
 	})
-	d.Visit(URL + "/contests/" + name + "/tasks")
+
+	d.Visit(URL + "/contests/" + contest.Base + "/tasks")
 }
 
-
+func Init(contestName string) *Contest {
+	err := os.Mkdir(contestName, 0777)
+	if err != nil {
+		log.Fatal(err.Error())
+		return nil
+	}
+	err = os.Chdir(contestName)
+	if err != nil {
+		log.Fatal(err.Error())
+		return nil
+	}
+	return &Contest{
+		Name:  "",
+		Base:  contestName,
+		Colly: colly.NewCollector(),
+	}
+}
 
 func main() {
-	folder := flag.String("dir",".","Select Directory")
-	contest := flag.String("contest","","Contest Name")
+	folder := flag.String("dir", ".", "Select Directory")
+	contest := flag.String("contest", "", "Contest Name")
 	flag.Parse()
 	if *contest == "" {
 		fmt.Println("Usage:")
@@ -92,8 +184,8 @@ func main() {
 		os.Exit(1)
 	}
 	os.Chdir(*folder)
-	c := colly.NewCollector()
-	os.Mkdir(*contest,0777)
-	os.Chdir(*contest)
-	getProblems(c,*contest)
+	atcoder := Init(*contest)
+	if atcoder != nil {
+		atcoder.ScrapeAtcoder()
+	}
 }
